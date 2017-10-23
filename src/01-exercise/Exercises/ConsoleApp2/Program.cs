6995df45b23180722a94df218acac33d71af338b
eerotpt <eero.sakson@tptlive.ee>
2017-10-23 11:35:22 +0200

    exercise_3

---
+++ src/01-exercise/Exercises/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sisestus;
+            Console.WriteLine("Mina olen papagoi programm.Ütle mulle midagi ja ma kordan selle sulle tagasi...");
+            Console.Write(Environment.NewLine);
+            Console.Write("Sina ütled: ");
+            sisestus = Console.ReadLine();
+            Console.Write("Mina ütlen: ");
+            Console.WriteLine(sisestus);
+            Console.ReadLine();
+
+        }
+    }
+}
