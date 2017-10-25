1a17c89665285cc35417c41f54cbd0a85e110c0f
eerotpt <eero.sakson@tptlive.ee>
2017-10-25 10:35:51 +0200

    03_dice_entry_2

--- src/03-dice-roller/DiceRoller/03_dice_roller/Program.cs
+++ src/03-dice-roller/DiceRoller/03_dice_roller/Program.cs
@@ -15,7 +15,12 @@ namespace _03_dice_roller
             Console.WriteLine("1d6: 5");
             Console.WriteLine("1d6: 4");
             Console.WriteLine("1d6: 1");
-
+            Console.WriteLine("1d8: 1");
+            Console.WriteLine("1d8: 6");
+            Console.WriteLine("");
+            Console.WriteLine("Roll total: 20");
+            Console.WriteLine("");
+            Console.WriteLine(">");
             Console.ReadLine();
         }
     }
