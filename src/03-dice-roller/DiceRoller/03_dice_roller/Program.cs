a9961f684b8f1f7dcc6164a0569179ab489e3b82
eerotpt <eero.sakson@tptlive.ee>
2017-10-25 10:31:57 +0200

    dice_1_entry

---
+++ src/03-dice-roller/DiceRoller/03_dice_roller/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_dice_roller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">");
+            Console.WriteLine("");
+            Console.WriteLine("1d6: 5");
+            Console.WriteLine("1d6: 4");
+            Console.WriteLine("1d6: 1");
+
+            Console.ReadLine();
+        }
+    }
+}
